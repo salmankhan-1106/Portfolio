a034b89e6c29111c7328f84b7dabb65a7ead9a45
Muhammad Salman Khan <m.salmankhan1106@gmail.com>
2025-07-04 20:40:07 +0500

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/interactive-portfolio/',
+  base: '/Portfolio/',
   server: {
     host: true,
     port: 3000,
